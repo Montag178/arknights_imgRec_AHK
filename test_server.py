827715327f12ef0814e4c2fd9b7373cb58c89480
Montag178 <montana7178@gmail.com>
2025-10-09 23:00:03 +0900

    before branch

--- test_server.py
+++ test_server.py
@@ -1,33 +1,65 @@
 # server.py
+import threading
 import os
 import win32pipe, win32file, pywintypes
+import sys
 
-PIPE_NAME = r'\\.\pipe\MyPipe'
+stop_event = threading.Event()
 
-print("Python server running. Waiting for AHK...")
-
-while True:
-    try:
-        # パイプを作って待ち受け
-        pipe = win32pipe.CreateNamedPipe(
-            PIPE_NAME,
+class PipeServer():
+    def __init__(self, pipeName):
+        self.pipe = win32pipe.CreateNamedPipe(
+            r'\\.\pipe\{}'.format(pipeName),
             win32pipe.PIPE_ACCESS_DUPLEX,
             win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
             1, 65536, 65536,
             0,
-            None
-        )
+            None)
+        self.pipename = pipeName
 
-        win32pipe.ConnectNamedPipe(pipe, None)  # AHKから接続が来るまでブロック
+    #Careful, this blocks until a connection is established
+    def connect(self):
+        win32pipe.ConnectNamedPipe(self.pipe, None)
 
-        # メッセージを受信
-        hr, data = win32file.ReadFile(pipe, 64*1024)
-        message = data.decode("utf-8").strip()
+    #Message without tailing '\n'
+    def write(self, message):
+        win32file.WriteFile(self.pipe, message.encode() + b'\n')
+
+    def read(self, size=64*1024):
+        hr, data = win32file.ReadFile(self.pipe, size)
+        return data.decode("utf-8").strip()
+
+    def close(self):
+        win32file.CloseHandle(self.pipe)
+
+def server():
+    try:
+        pipe = PipeServer("MyPipe")
+        pipe.connect()
+        message = pipe.read()
         if message:
             print("Received:", message)
             print("hello, world")  # 合図を受けたら出力
-
-        win32file.CloseHandle(pipe)
+        pipe.close()
 
     except pywintypes.error as e:
         print("Error:", e)
+
+
+def main():
+    server_thread = threading.Thread(target=server, daemon=True)
+    server_thread.start()
+    print("Server start")
+
+    try:
+        while not stop_event.is_set():
+            stop_event.wait(timeout=0.1)
+    except KeyboardInterrupt:
+        print("\nCtrl+C detected. Exiting gracefully")
+        stop_event.set()
+        sys.exit(0)
+        # メインスレッドが終了するとデーモンスレッドも終了する
+    
+
+if __name__ == "__main__":
+    main()
